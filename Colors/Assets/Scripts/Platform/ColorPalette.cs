86e1079b068100305e5834b5509c6aaaddfab4fb
AngyBlacwood <saldina_2011@mail.ru>
2022-06-18 19:03:38 +0500

    added basic coloring platforms mechanic

---
+++ Colors/Assets/Scripts/Platform/ColorPalette.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPalette : MonoBehaviour
+{
+    [SerializeField] private Color[] colors;
+    [SerializeField] private Image potion;
+    private int selectedColor;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            selectedColor += 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            selectedColor -= 1;
+        }
+        selectedColor %= colors.Length;
+        potion.color = GetSelectedColor();
+    }
+
+    public Color GetSelectedColor()
+    {
+        return colors[Math.Abs(selectedColor)];
+    }
+}
